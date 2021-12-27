36947a9ec13ed6a3b276839da3ef1188cada5948
Aalmani1 <mem.mem6789@gmail.com>
2021-12-27 14:59:34 +0300

    done display

--- perfect-perfumes/frontend/src/component/userView/Display.js
+++ perfect-perfumes/frontend/src/component/userView/Display.js
@@ -25,7 +25,7 @@ const Toast = MySwal.mixin({
 
 function Display() {
   const { id } = useParams();
-  const [product, setproduct] = useState([]);
+  const [product, setproduct] = useState({});
   const [addItem, setAddItem] = useState([]);
   const [allProduct, setAllProduct] = useState([]);
 
@@ -92,33 +92,39 @@ function Display() {
     });
   }, []);
   let arr = [];
-  let opj = [product.img , product.brand  , product.name , product.description , product.gender,product.size,product.price]
+  let opj = {
+    product,
+  };
   return (
     <div>
       <div className="container">
-        <div className="display-img">
-          <img className="imd-display" src={product.img} />
-        </div>
-        <div className="display-content">
-          <h3>{product.brand}</h3>
-          <br></br>
-          <h2>{product.name}</h2>
-          <p>{product.description} </p>
-          <h6>For {product.gender} </h6>
-          <h6>Size: {product.size} </h6>
-          <h4> {product.price} SRA</h4>
-          <br></br>
-          <br></br>
-          <br></br>
-          <Button
-            onClick={() => {
-              addToCart(opj);
-            }}
-            id="loginbtn"
-          >
-            Buy Now
-          </Button>
-        </div>
+        {product && (
+          <>
+            <div className="display-img">
+              <img className="imd-display" src={product.img} />
+            </div>
+            <div className="display-content">
+              <h3>{product.brand}</h3>
+              <br></br>
+              <h2>{product.name}</h2>
+              <p>{product.description} </p>
+              <h6>For {product.gender} </h6>
+              <h6>Size: {product.size} </h6>
+              <h4> {product.price} SRA</h4>
+              <br></br>
+              <br></br>
+              <br></br>
+              <Button
+                onClick={() => {
+                  addToCart(product);
+                }}
+                id="loginbtn"
+              >
+                Buy Now
+              </Button>
+            </div>
+          </>
+        )}
       </div>
 
       <div className="header-title2">
@@ -145,9 +151,14 @@ function Display() {
                     <Card.Text style={{ textAlign: "center" }}>
                       Price: {item.price} SRA
                     </Card.Text>
-                    <Button          onClick={() => {
-                      addToCart(item);
-                    }} id="loginbtn">Add To Cart</Button>
+                    <Button
+                      onClick={() => {
+                        addToCart(item);
+                      }}
+                      id="loginbtn"
+                    >
+                      Add To Cart
+                    </Button>
                   </Card.Body>
                 </CardGroup>
               );
