34d67fc424751676c77e73ec08d07b610932341f
Aalmani1 <mem.mem6789@gmail.com>
2021-12-28 10:38:38 +0300

    test

--- app.js
+++ app.js
@@ -53,18 +53,17 @@ app.post("/payment", (req, res) => {
 
 app.get("*", (req, res) => {
   console.log(path.resolve(__dirname, "frontend/build/index.html"));
-  res.sendFile(path.resolve(__dirname, "frontend/build", "index.html"));
+  res.sendFile(path.resolve(__dirname + "/frontend/build", "index.html"));
 });
 
-app.listen(process.env.PORT || 3001, () => {
-  console.log("app work");
-  if (process.env.NODE_ENV === "test") app.set(PORT, 3001);
-  else app.set(PORT, process.env.PORT || 3000);
-});
+// app.listen(process.env.PORT || 3001, () => {
+//   console.log("app work");
+//   if (process.env.NODE_ENV === "test") app.set(PORT, 3001);
+//   else app.set(PORT, process.env.PORT || 3000);
+// });
 
 // app.listen(PORT, () => {
 //   console.log(`Connected on= http://localhost:${PORT}`);
 // });
 
-// const PORT = process.env.PORT || 3001;
-// app.listen(PORT);
+app.listen(PORT);
